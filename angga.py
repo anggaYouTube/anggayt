ae8271acb07217ce7519ff55d26a5227b92bf2fe
AnggaYouTube <58868017+anggaYouTube@users.noreply.github.com>
2020-01-03 04:24:21 +0700

    Update angga.py

--- angga.py
+++ angga.py
@@ -1,4 +1,28 @@
-#Decompiled by MR.K7C8NG
+#!/bin/bash
+#version 1.0
+
+clear
+# Variable warna
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+cowsay -f eyes " AnggaYT" | lolcat
+figlet -f slant "AnggaYT" | lolcat
+echo "    <=====================[]====================>" | lolcat
+echo "    <=====[       Tools By AnggaYT        ]=====>" | lolcat
+echo "    <=====[  Concact Me : 08976341427     ]=====>" | lolcat
+echo "    <=====================[]====================>" | lolcat
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
